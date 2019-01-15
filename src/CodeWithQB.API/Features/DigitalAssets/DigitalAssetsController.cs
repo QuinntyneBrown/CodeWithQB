2d29a4c97fec6ce13c940df2ad17a6ba07409319
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-01-15 00:11:33 -0500

    add digital assets controller

---
+++ src/CodeWithQB.API/Features/DigitalAssets/DigitalAssetsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CodeWithQB.API.Features.DigitalAssets
+{
+    [ApiController]
+    [Route("api/digitalassets")]
+    public class DigitalAssetsController
+    {
+        public async Task<ActionResult<string>> Get() {
+            return await Task.FromResult("Digital Assets");
+        }
+    }
+}
