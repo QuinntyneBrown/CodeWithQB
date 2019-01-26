bba2dc8bcd01926607c1a8af6e933acac2aba016
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-01-26 03:29:08 -0500

    refactor

---
+++ src/CodeWithQB.API/Features/Addresses/AddressExtensions.cs
@@ -0,0 +1,17 @@
+﻿using CodeWithQB.API.Features.Addresses;
+using CodeWithQB.Core.Models;
+
+namespace CodeWithQB.API.Features.Addresses
+{
+    public static class AddressExtensions
+    {
+        public static AddressDto ToDto(this Address x)
+            => new AddressDto
+            {
+                Street = x.Street,
+                City = x.City,
+                Province = x.Province,
+                PostalCode = x.PostalCode
+            };
+    }
+}
