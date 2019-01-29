5d581677aafffcfbe90a2cac4a469c1e5f9bd107
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-01-28 23:36:01 -0500

    add tags to home page view model

--- src/CodeWithQB.API/BFF/HomePage/HomePageViewModel.cs
+++ src/CodeWithQB.API/BFF/HomePage/HomePageViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace CodeWithQB.API.BFF.HomePage
+﻿using CodeWithQB.API.Features.Tags;
+using System.Collections.Generic;
+
+namespace CodeWithQB.API.BFF.HomePage
 {
     public class HomePageViewModel
     {
         public string Title { get; set; }
         public string FullName { get; set; }
         public string ImageUrl { get; set; }
+        public ICollection<TagDto> Tags { get; set; }
+            = new HashSet<TagDto>();
     }
 }
