c00c9d08ea0f37f11e1614dfa307666baf6d227c
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2019-01-30 18:19:38 -0500

    use cancellation tokens

--- src/CodeWithQB.API/Features/Customers/CustomersController.cs
+++ src/CodeWithQB.API/Features/Customers/CustomersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CodeWithQB.API.Features.Customers
@@ -18,25 +19,25 @@ namespace CodeWithQB.API.Features.Customers
         [HttpGet]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         [ProducesResponseType(typeof(GetCustomersQuery.Response), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<GetCustomersQuery.Response>> Get()
-            => await _meditator.Send(new GetCustomersQuery.Request());
+        public async Task<ActionResult<GetCustomersQuery.Response>> Get(CancellationToken cancellationToken)
+            => await _meditator.Send(new GetCustomersQuery.Request(), cancellationToken);
 
         [HttpGet("{customerId}")]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         [ProducesResponseType(typeof(GetCustomerByIdQuery.Response), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<GetCustomerByIdQuery.Response>> GetById(GetCustomerByIdQuery.Request request)
-            => await _meditator.Send(request);
+        public async Task<ActionResult<GetCustomerByIdQuery.Response>> GetById(GetCustomerByIdQuery.Request request, CancellationToken cancellationToken)
+            => await _meditator.Send(request, cancellationToken);
 
         [HttpPost]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         [ProducesResponseType(typeof(UpsertCustomerCommand.Response), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<UpsertCustomerCommand.Response>> Upsert(UpsertCustomerCommand.Request request)
-            => await _meditator.Send(request);
+        public async Task<ActionResult<UpsertCustomerCommand.Response>> Upsert(UpsertCustomerCommand.Request request, CancellationToken cancellationToken)
+            => await _meditator.Send(request, cancellationToken);
 
         [HttpDelete]
         [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         [ProducesResponseType(typeof(Unit), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<Unit>> Upsert([FromQuery]RemoveCustomerCommand.Request request)
-            => await _meditator.Send(request);
+        public async Task<ActionResult<Unit>> Upsert([FromQuery]RemoveCustomerCommand.Request request, CancellationToken cancellationToken)
+            => await _meditator.Send(request, cancellationToken);
     }
 }
